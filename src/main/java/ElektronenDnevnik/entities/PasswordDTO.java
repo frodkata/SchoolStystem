a194f23e7039e58c69bba7770230461d34e7bacc
frodkata <fordkata@gmail.com>
2021-02-11 08:05:24 +0200

    Change password transfer for form

---
+++ src/main/java/ElektronenDnevnik/entities/PasswordDTO.java
@@ -0,0 +1,22 @@
+package ElektronenDnevnik.entities;
+
+public class PasswordDTO {
+    String oldPassword;
+    String newPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
